14315cc34aa901850304d6698928231669d120c2
stansfield <aaronmstansfield@gmail.com>
2025-11-29 15:34:08 +0000

    finished implementing the fast forward thing
    havent tested it in vr yet tho hopefully it mostly works

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: constructy test
 - Unity version: Unity 6000.0.55f1
 - Active game object:
-  - Name: Text (TMP)
-  - Tag: Untagged
-  - Layer: UI
+  - Name: Falling complete
+  - Tag: hazard
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/ourAssets/scrips/gamerManage.cs
+++ Assets/ourAssets/scrips/gamerManage.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Authentication.ExtendedProtection;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 
 public class gamerManage : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class gamerManage : MonoBehaviour
     [SerializeField] GameObject[] situations;
 
     public Hashtable reports = new Hashtable();
+
+    private GameObject[] situationList = new GameObject[12];
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,7 +39,8 @@ public class gamerManage : MonoBehaviour
         int i = 0;
         foreach (GameObject situation in situations)
         {
-            reports[i] = situation.GetComponent<situationScrip>().report();
+            situationList[i] = situation;
+            reports[i] = situation.GetComponentInChildren<situationScrip>().report();
             i++;
         }
 
@@ -59,6 +63,30 @@ public class gamerManage : MonoBehaviour
             }
             // report for each scenario (just the ticket check for now)
             reportText.text += ($"{situations[j].name}: ID - {(intermediate[0] ? "Identified" : "Missed")}, \n Fixed - {(intermediate[1] ? "Yes" : "No")}\n");
+
+            situationList[j].transform.GetChild(0).gameObject.SetActive(false);
+            if (intermediate[0])
+            {
+                situationList[j].transform.GetChild(1).gameObject.SetActive(true);
+            }
+            else
+            {
+
+                try
+                {
+                    if (situationList[j].transform.GetChild(2).transform != null)
+                    {
+
+                        situationList[j].transform.GetChild(2).gameObject.SetActive(true);
+                    }
+                }
+
+                catch
+                {
+                    situationList[j].transform.GetChild(1).gameObject.SetActive(true);
+                }
+
+            }
         }
         
         //total score out of total number of scenarios
