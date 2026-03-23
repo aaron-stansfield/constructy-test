7287590211ec4fdb9fa6bd41333e74236e83ae79
stansfield <aaronmstansfield@gmail.com>
2026-03-23 20:35:55 +0000

    stuff
    imported the tree and floor assets
    added a proper start menu scene
    added the hdri thing and messed with the lighting
    replaced the gun with the new asset

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: constructy test
 - Unity version: Unity 6000.0.55f1
 - Active game object:
-  - Name: Untitled:Untitled:Mesh
-  - Tag: Occluder Static
+  - Name: sceneManager
+  - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Assets/Scripts/sceneManager.cs
@@ -0,0 +1,30 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void startMainScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
